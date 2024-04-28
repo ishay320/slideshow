29659fda077c2c7ed73bd0d706582f98752c1b08
ishay320 <ishay320@hotmail.com>
2024-04-28 17:04:22 +0300

    Made the file getter work

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
+#include <stdlib.h>
+
 #include <filesystem>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 #include "commandline.h"
+#include "logger.h"
 #include "raylib.h"
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -16,17 +20,31 @@ std::vector<std::string> saparateByDelim(const std::string& s,
     size_t pos  = 0;
     size_t last = 0;
     while ((pos = s.find(delim, last)) != std::string::npos) {
-        out.push_back(s.substr(last, pos));
+        out.push_back(s.substr(last, pos - last));
         last = pos + 1;
     }
     return out;
 }
 
-std::vector<std::filesystem::path> getFilesByRegex(
-    const std::filesystem::path& path, const std::string& regex)
+std::vector<std::filesystem::path> getFilesByTypes(
+    const std::filesystem::path& path, const std::vector<std::string>& types)
 {
+    std::vector<std::string> search_prompt;
+    if (types.empty()) {
+        throw std::runtime_error{"no types passed"};
+    }
+
+    search_prompt.push_back(".");
+    search_prompt.push_back("-iname");
+    search_prompt.push_back("*\\." + types[0]);
+    for (size_t i = 1; i < types.size(); i++) {
+        search_prompt.push_back("-o");
+        search_prompt.push_back("-iname");
+        search_prompt.push_back("*\\." + types[i]);
+    }
+
     std::vector<std::filesystem::path> out;
-    std::string c_out = run_command_with_out("find", {path, "-name", regex});
+    std::string c_out = run_command_with_out("find", search_prompt);
     for (auto&& s : saparateByDelim(c_out, "\n")) {
         out.push_back(s);
     }
@@ -37,10 +55,20 @@ std::vector<std::filesystem::path> getFilesByRegex(
 // get image (char**)
 class FileGetter {
    public:
-    FileGetter();
-    ~FileGetter();
+    FileGetter(const std::string& path, const std::vector<std::string>& types)
+    {
+        _paths = getFilesByTypes(path, types);
+    }
+    ~FileGetter() = default;
 
-    std::filesystem::path getNext();
+    std::filesystem::path getNext()
+    {
+        int pos = rand() % _paths.size();
+        return _paths[pos];
+    }
+
+   private:
+    std::vector<std::filesystem::path> _paths;
 };
 
 class TextureWrapper {
@@ -87,8 +115,8 @@ int main(int argc, char* argv[])
     ToggleFullscreen();
     SetTargetFPS(30);
 
-    std::vector<std::filesystem::path> files = getFilesByRegex(".", "*\\.jpg");
-    TextureWrapper image{files[0]};
+    FileGetter fg{".", {"png", "jpg"}};
+    TextureWrapper image{fg.getNext()};
 
     int pos_x = 0, pos_y = 0;
     while (!WindowShouldClose()) {
