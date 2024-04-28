70a15209dc94aa1fb1a8611273afba99437c7b20
ishay320 <ishay320@hotmail.com>
2024-04-28 15:08:20 +0300

    Added todo and command line functions

--- main.cpp
+++ main.cpp
@@ -1,32 +1,106 @@
+#include <filesystem>
 #include <iostream>
+#include <string>
+#include <vector>
 
+#include "commandline.h"
 #include "raylib.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-int main(int argc, char *argv[])
+std::vector<std::string> saparateByDelim(const std::string& s,
+                                         const std::string& delim)
+{
+    std::vector<std::string> out;
+    size_t pos  = 0;
+    size_t last = 0;
+    while ((pos = s.find(delim, last)) != std::string::npos) {
+        out.push_back(s.substr(last, pos));
+        last = pos + 1;
+    }
+    return out;
+}
+
+std::vector<std::filesystem::path> getFilesByRegex(
+    const std::filesystem::path& path, const std::string& regex)
+{
+    std::vector<std::filesystem::path> out;
+    std::string c_out = run_command_with_out("find", {path, "-name", regex});
+    for (auto&& s : saparateByDelim(c_out, "\n")) {
+        out.push_back(s);
+    }
+    return out;
+}
+
+// get random file
+// get image (char**)
+class FileGetter {
+   public:
+    FileGetter();
+    ~FileGetter();
+
+    std::filesystem::path getNext();
+};
+
+class TextureWrapper {
+   public:
+    TextureWrapper(const std::filesystem::path& path)
+    {
+        _texture = LoadTexture(path.string().c_str());
+        if (_texture.id <= 0) {
+            throw std::filesystem::filesystem_error("could not load image",
+                                                    path, std::error_code());
+        }
+    }
+    ~TextureWrapper() { UnloadTexture(_texture); }
+
+    Texture texture() { return _texture; }
+
+   private:
+    Texture _texture;
+};
+
+class GetTextureWrapper {
+   public:
+    GetTextureWrapper(FileGetter files_getter) : _files_getter(files_getter) {}
+    ~GetTextureWrapper() = default;
+
+    TextureWrapper getNext();
+
+   private:
+    FileGetter _files_getter;
+};
+
+// steps:
+// [ ] 1. check updates online
+// [ ] 2. check config for folders of images
+// [ ] 3. create class of files
+// [ ] 4. create class of texture getter
+// [ ] 5. create class of effect
+// [ ] 6. loop til the world end
+int main(int argc, char* argv[])
 {
     InitWindow(800, 450, "slideshow");
+    const int display = GetCurrentMonitor();
+    SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+    ToggleFullscreen();
     SetTargetFPS(30);
 
-    Texture2D image = LoadTexture("./data/test.jpg");
-    if (image.id <= 0) {
-        std::cout << "could not load image\n";
-        return 1;
-    }
+    std::vector<std::filesystem::path> files = getFilesByRegex(".", "*\\.jpg");
+    TextureWrapper image{files[0]};
 
     int pos_x = 0, pos_y = 0;
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawTexture(image, pos_x++, pos_y++, WHITE);
+        DrawTexture(image.texture(), pos_x++, pos_y++, WHITE);
 
         DrawFPS(50, 50);
         EndDrawing();
     }
 
-    UnloadTexture(image);
     CloseWindow();
     return 0;
 }
+// TODO: add logger
