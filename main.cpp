39ebe99c45aea67b1bbd23abd73ec9021ba2c733
ishay320 <ishay320@hotmail.com>
2024-05-01 23:54:08 +0300

    Upgraded the effect to have background and on the way the wrappers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 
 #include <filesystem>
-#include <iostream>
 #include <memory>
 #include <random>
 #include <stdexcept>
@@ -71,11 +70,38 @@ class FileGetter {
     std::vector<std::filesystem::path> _paths;
 };
 
+class ImageWrapper {
+   public:
+    ImageWrapper(Image image)
+    {
+        _image = std::shared_ptr<Image>(new Image(image),
+                                        [](Image* t) { UnloadImage(*t); });
+    }
+    ImageWrapper(const std::filesystem::path& path)
+    {
+        Image image = LoadImage(path.c_str());
+        // ImageFormat(&imOrigin, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8)
+        _image = std::shared_ptr<Image>(new Image(image),
+                                        [](Image* t) { UnloadImage(*t); });
+    }
+    ImageWrapper(ImageWrapper&&)                 = default;
+    ImageWrapper(const ImageWrapper&)            = default;
+    ImageWrapper& operator=(ImageWrapper&&)      = default;
+    ImageWrapper& operator=(const ImageWrapper&) = default;
+    ~ImageWrapper()                              = default;
+
+    Image get() const { return *_image.get(); }
+    void blur(int size) { ImageBlurGaussian(_image.get(), size); }
+
+   private:
+    std::shared_ptr<Image> _image;
+};
+
 class TextureWrapper {
    public:
-    TextureWrapper(const std::filesystem::path& path)
+    TextureWrapper(const std::filesystem::path& path) : _image(path)
     {
-        Texture texture = LoadTexture(path.string().c_str());
+        Texture texture = LoadTextureFromImage(_image.get());
         if (texture.id <= 0) {
             throw std::filesystem::filesystem_error("could not load image",
                                                     path, std::error_code());
@@ -84,11 +110,35 @@ class TextureWrapper {
             new Texture(texture), [](Texture* t) { UnloadTexture(*t); });
     }
 
+    TextureWrapper(Image image) : _image(image)
+    {
+        Texture texture = LoadTextureFromImage(image);
+        if (texture.id <= 0) {
+            throw "could not load image to gpu";
+        }
+        _texture = std::shared_ptr<Texture>(
+            new Texture(texture), [](Texture* t) { UnloadTexture(*t); });
+    }
     ~TextureWrapper() {}
 
     Texture texture() const { return *_texture.get(); }
 
+    TextureWrapper copy() const
+    {
+        Image image = ImageCopy(_image.get());
+        return {image};
+    }
+
+    void blur(int size)
+    {
+        _image.blur(size);
+        Texture texture = LoadTextureFromImage(_image.get());
+        _texture        = std::shared_ptr<Texture>(
+            new Texture(texture), [](Texture* t) { UnloadTexture(*t); });
+    }
+
    private:
+    ImageWrapper _image;
     std::shared_ptr<Texture> _texture;
 };
 
@@ -143,30 +193,55 @@ Point getRandomPointAndVelocity(double radius, double speed)
 
 class Effect {
    public:
-    Effect(TextureWrapper texture) : _texture(texture)
+    Effect(TextureWrapper texture)
+        : _texture(texture), _texture_back(_texture.copy())
     {
+        const int display  = GetCurrentMonitor();
+        const int width_s  = GetMonitorWidth(display);
+        const int height_s = GetMonitorHeight(display);
+
+        const int middle_x = width_s / 2;
+        const int middle_y = height_s / 2;
+
+        const int width_i  = _texture.texture().width;
+        const int height_i = _texture.texture().height;
+
         _point = getRandomPointAndVelocity(200, 100);
+        _point.x += middle_x - (width_i / 2.f);
+        _point.y += middle_y - (height_i / 2.f);
+        _scale = width_i < height_i ? (float)width_s / width_i
+                                    : (float)height_s / height_i;
+        _texture_back.blur(10);
     }
-    Effect(Effect const& e) : _point(e._point), _texture(e._texture) {}
+    Effect(Effect const& e)
+        : _point(e._point), _texture(e._texture), _texture_back(e._texture_back)
+    {
+    }
     Effect& operator=(const Effect& e)
     {
-        _point   = e._point;
-        _texture = e._texture;
+        _point        = e._point;
+        _texture      = e._texture;
+        _texture_back = e._texture_back;
         return *this;
     }
     ~Effect() = default;
 
     void update(double delta_time)
     {
+        DrawTextureEx(_texture_back.texture(), {0, 0}, 0, _scale, WHITE);
         DrawTexture(_texture.texture(), static_cast<int>(_point.x),
                     static_cast<int>(_point.y), WHITE);
         _point.x += _point.vx * delta_time;
         _point.y += _point.vy * delta_time;
     }
 
+    TextureWrapper getTexture() const { return _texture; }
+
    private:
+    float _scale;
     Point _point;
     TextureWrapper _texture;
+    TextureWrapper _texture_back;
 };
 
 // steps:
@@ -176,26 +251,30 @@ class Effect {
 // [x] 4. create class of texture getter
 // [x] 5. create class of effect
 // [ ] 6. create class of transition- it fill them with the image and transition
-// [ ] 7. loop til the world end
+// [x] 7. loop til the world end
 int main(int argc, char* argv[])
 {
     struct {
         int swap_time = 5;
         std::vector<std::string> file_types{"png", "jpg"};
-        int fps = 30;
+        int fps                 = 30;
+        std::string images_path = ".";
     } config;
 
+    LOG_INFO("starting window");
     InitWindow(800, 450, "slideshow");
     const int display = GetCurrentMonitor();
     SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
     SetTargetFPS(config.fps);
     ToggleFullscreen();
 
-    FileGetter fg{".", config.file_types};
+    LOG_INFO("loading files from '" << config.images_path << '\'');
+    FileGetter fg{config.images_path, config.file_types};
     GetTextureWrapper gtw{fg};
     TextureWrapper image = gtw.getNext();
     Effect effect{image};
 
+    LOG_INFO("starting the loop");
     int next_swap          = config.swap_time;
     double prev_frame_time = GetTime();
     while (!WindowShouldClose()) {
@@ -215,7 +294,7 @@ int main(int argc, char* argv[])
         }
     }
 
+    LOG_INFO("closing the window");
     CloseWindow();
     return 0;
 }
-// TODO: add logger
